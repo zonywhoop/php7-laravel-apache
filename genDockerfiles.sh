efccce6731b8f1cb1b05c6257b713906b10b58f3
Ed McLain <emclain@digitalmotion.tech>
2019-04-27 12:07:05 -0500

    Move to use debian stretch version, some clean up, and addition of php 7.2

--- genDockerfiles.sh
+++ genDockerfiles.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-PHPFROM=(7.0 7.1)
+PHPFROM=(7.0 7.1 7.2)
 
 for phpver in ${PHPFROM[*]}; do 
     echo "Building Dockerfile for PHP ${phpver}"
-    dockerFrom="php:${phpver}-apache"
+    dockerFrom="php:${phpver}-apache-stretch"
     dockerAdd=""
     
+    if [ "$phpver" == "7.2" ]; then
+      phpmods="bcmath xml zip pdo_mysql mysqli sockets pcntl"
+    else
+      phpmods="bcmath mcrypt xml zip pdo_mysql mysqli sockets pcntl"
+    fi
+
     if [ ! -d "${phpver}" ]; then
       mkdir ${phpver}
     fi
@@ -18,7 +24,9 @@ WORKDIR /var/www/laravel
 
 ${dockerAdd}
 
-RUN apt-get update && apt-get -y install \
+RUN apt-get update \
+    && apt-get -y upgrade \
+    && apt-get -y install --no-install-recommends \
         libfreetype6-dev \
         libjpeg62-turbo-dev \
         libmcrypt-dev \
@@ -27,18 +35,22 @@ RUN apt-get update && apt-get -y install \
         libcurl4-gnutls-dev \
         zlib1g-dev \
         git \
-    && docker-php-ext-install -j2 bcmath mcrypt xml mbstring json curl zip pdo_mysql sockets pcntl \
+        unzip \
+    && docker-php-ext-install -j2 $phpmods \
     && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
     && docker-php-ext-install -j2 gd \
     && echo \$(printf "\n" | pecl install redis) \
     && docker-php-ext-enable redis \
     && cd /usr/src/ \
     && ln -s /etc/apache2/mods-available/rewrite.* /etc/apache2/mods-enabled/ \
-    && apt-get remove --purge -y libssl-dev libssl-doc \
-    && rm -rf /var/lib/apt/lists/* \
     && curl -o /tmp/composer_setup https://getcomposer.org/installer \
     && php /tmp/composer_setup --install-dir=/usr/local/bin --filename=composer \
     && composer global require "laravel/installer" 
+    && apt-get remove --purge -y libssl-dev libssl-doc \
+    && rm -rf /var/lib/apt/lists/* \
+    && apt-get clean autoclean \
+    && apt-get autoremove -y \
+    && rm -rf /var/lib/apt/lists/* /tmp/* /var/lib/dpkg/* /var/lib/cache/* /var/lib/log/*
 
 COPY docker/apache-laravel.conf /etc/apache2/sites-enabled/000-default.conf
 
